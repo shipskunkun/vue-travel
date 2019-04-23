5ebf0c2d617a773b92ff29f8cd0d9533da3c5e21
Kunhua Zhang(张坤华) <zhangkunhua@le.com>
2019-04-24 01:36:27 +0800

    update to 6-5

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+
 Vue.config.productionTip = false
 
 new Vue({
