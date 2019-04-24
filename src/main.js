149f522be59a49512fc4025ab596f78863f50325
Kunhua Zhang(张坤华) <zhangkunhua@le.com>
2019-04-24 22:40:18 +0800

    从头来过 回到1-1章 最初形态

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-
-import 'styles/reset.css'
-import 'styles/border.css'
-import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
