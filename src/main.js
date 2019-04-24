4bb1ceb6c9008a3d601a6f6a33b78d5a7be1687d
Kunhua Zhang(张坤华) <zhangkunhua@le.com>
2019-04-25 01:15:06 +0800

    4-25 前6章review done 项目可跑

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import fastClick from 'fastclick'
+
+
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
+
+fastClick.attach(document.body)
 
 new Vue({
   render: h => h(App)
