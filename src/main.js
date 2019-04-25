261d72b8a30aa67e7aa73eed193f95d84a0d4daa
Kunhua Zhang(张坤华) <zhangkunhua@le.com>
2019-04-25 23:45:38 +0800

    4.25 upto 7-3章

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import fastClick from 'fastclick'
+import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 
 fastClick.attach(document.body)
 
+
+Vue.use(VueAwesomeSwiper)
+fastClick.attach(document.body)
+
+
+
 new Vue({
-  render: h => h(App)
+	el:' #app',
+	router,
+  	render: h => h(App)
 }).$mount('#app')
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <home-header></home-header>
+      <home-swiper></home-swiper>
+    </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper
+  },
+  data () {
+    return {
+
+    }
+  },
+
+  methods: {
+  },
+  mounted () {
+  },
+  activated () {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
